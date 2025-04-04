a594f44cca3e1d7c2abe6aa84b744d8ba669756a
Avril Rasic <rasicavril31@gmail.com>
2025-04-04 11:17:52 -0300

    Agregado puntaje, mvp y tabla arreglada

--- src/contar.py
+++ src/contar.py
@@ -1,23 +1,43 @@
 def contar_rondas (rounds):
     final = {}
     num = 0
+    kills = 0
     for round  in rounds: 
         for personaje,datos in round.items():
         #Si no cree el dicc del personaje
             if personaje not in final:
-                final[personaje] = {'kills': 0, 'assists': 0,'deaths': 0}
+                final[personaje] = {'kills': 0, 'assists': 0,'deaths': 0,'mvp':0,'puntaje':0}
 
         #Aumento los datos 
             final[personaje]['kills'] += datos['kills']
             final[personaje]['assists'] += datos['assists']
             if datos['deaths'] == True: 
                 final[personaje]['deaths'] += datos['deaths']
-        num = num + 1
 
+        #puntaje
+            contar = 0
+            if (datos['kills']>0):
+                contar += datos['kills']*3
+            if(datos['assists']>0):
+                contar += datos['assists']
+            if(datos['deaths'] == True):
+                contar -= 1
+            final[personaje]['puntaje'] = contar
+
+    #MVP de la ronda
+        puntaje = 0
+        for jugador, datos in final.items(): 
+            if datos['puntaje']>puntaje: 
+                puntaje = datos['puntaje']
+                nombre = jugador
+        final[nombre]['mvp'] += 1
+        
+
+        num = num + 1
         print(f"Resultado ronda {num}")
-        print(f"|{'Personaje':<9}|{'Kills':<5}|{'Assists':<7}|{'Deaths':<6}|")
+        print(f"|{'Personaje':<9}|{'Kills':<5}|{'Assists':<7}|{'Deaths':<6}|{'Mvp':<6}|{'Puntos':<6}|")
         print(f"|{'-'*9}|{'-'*5}|{'-'*7}|{'-'*6}|")
         for personaje, datos in final.items():
-            print(f'|{personaje:<9}|{datos['kills']:<5}|{datos['assists']:<7}|{datos['deaths']:<6}|')
+            print(f'|{personaje:<9}|{datos['kills']:<5}|{datos['assists']:<7}|{datos['deaths']:<6}|{datos['mvp']:<3}|{datos['puntaje']:<6}')
         print("")
     return final
