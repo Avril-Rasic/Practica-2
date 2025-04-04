59e0a977bf90fe301de29aea94ffb5d63ed84ea8
Avril Rasic <rasicavril31@gmail.com>
2025-04-04 09:52:40 -0300

    Modificaciones contar

--- src/contar.py
+++ src/contar.py
@@ -1,17 +1,13 @@
-
-from imprimir import imprimir_ronda
-def contar_rondas (rounds):
+def contar_rondas (round):
     final = {}
-    for round in rounds: 
-        for personaje, datos in round.items():
+    for personaje, datos in round:
+        #Si no cree el dicc del personaje
+        if personaje not in final:
+            final[personaje] = {'kills': 0, 'assists': 0,'deaths': 0}
 
-            #Si no cree el dicc del personaje
-            if personaje not in final:
-                final[personaje] = {'kills': 0, 'assists': 0,'deaths': 0}
-
-            #Aumento los datos 
-            final[personaje]['kills'] += datos['kills']
-            final[personaje]['assists'] += datos['assists']
-            if datos['deaths'] == True: 
-                final[personaje]['deaths'] += datos['deaths']
+        #Aumento los datos 
+        final[personaje]['kills'] += datos['kills']
+        final[personaje]['assists'] += datos['assists']
+        if datos['deaths'] == True: 
+            final[personaje]['deaths'] += datos['deaths']
     return final
